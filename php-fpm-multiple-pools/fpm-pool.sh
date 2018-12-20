a8a5e56a23af65a04bef4a5cc0a61db327eef897
Maxim Leontyev <leontyevmax@gmail.com>
2018-12-20 16:58:39 +0400

    Added template for fpm with multiple pools support

---
+++ php-fpm-multiple-pools/fpm-pool.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+##################################
+# Zabbix monitoring script
+#
+# php-fpm:
+#  - anything available via FPM status page
+#
+##################################
+
+
+if [[ "$1" == "discovery" ]] ; then
+	echo '{"data":['
+	for file in `find /etc/php5/fpm/pool.d/ /etc/php/*/fpm/pool.d/ -name '*.conf' 2>/dev/null` ; do
+		poolname=`grep -e '^\s*\[[a-zA-Z-]*\]\s*$' $file | sed -e 's/.*\[\(.*\)\].*/\1/'`
+		status_path=`grep -e '^\s*pm\.status_path\s*=\s*' $file | awk -F '=' '{print $2;}' | sed -e 's/^\s*\([^ ;]*\)\s*;\?.*/\1/'`
+		if [[ "$comma" == 1 ]] ; then
+			echo ','
+		fi
+		echo '	{'
+		echo '		"{#POOL}":"'$poolname'",'
+		echo '		"{#STATUS}":"'$status_path'"'
+		echo -n '	}'
+		comma=1
+	done
+	echo ']}'
+	exit
+fi
+
+
+# Zabbix requested parameter
+ZBX_REQ_DATA="$1"
+ZBX_REQ_DATA_URL="$2"
+
+# Nginx defaults
+STATUS_HOST="http://127.0.0.1"
+NGINX_STATUS_DEFAULT_URL="/status"
+
+WGET_BIN="/usr/bin/wget"
+
+#
+# Error handling:
+#  - need to be displayable in Zabbix (avoid NOT_SUPPORTED)
+#  - items need to be of type "float" (allow negative + float)
+#
+ERROR_NO_ACCESS_FILE="-0.9900"
+ERROR_NO_ACCESS="-0.9901"
+ERROR_WRONG_PARAM="-0.9902"
+ERROR_DATA="-0.9903" # either can not connect /	bad host / bad port
+
+# Handle host and port if non-default
+if [ ! -z "$ZBX_REQ_DATA_URL" ]; then
+  URL="$ZBX_REQ_DATA_URL"
+else
+  URL="$NGINX_STATUS_DEFAULT_URL"
+fi
+
+URL=${STATUS_HOST}${URL}
+
+# save the nginx stats in a variable for future parsing
+NGINX_STATS=$($WGET_BIN -q $URL -O - 2> /dev/null)
+
+# error during retrieve
+if [ $? -ne 0 -o -z "$NGINX_STATS" ]; then
+  echo $ERROR_DATA
+  exit 1
+fi
+
+# 
+# Extract data from nginx stats
+#
+#RESULT=$(echo "$NGINX_STATS" | awk 'print $0;match($0, "^'"$ZBX_REQ_DATA"':[[:space:]]+(.*)", a) { print a[1] }')
+#RESULT=$(echo "$NGINX_STATS" | grep "$ZBX_REQ_DATA" | awk -F : '{print $2}')
+RESULT=$(echo "$NGINX_STATS" | awk -F : "{if(\$1==\"$ZBX_REQ_DATA\") print \$2}")
+if [ $? -ne 0 -o -z "$RESULT" ]; then
+    echo $ERROR_WRONG_PARAM
+    exit 1
+fi
+
+echo $RESULT
+
+exit 0
